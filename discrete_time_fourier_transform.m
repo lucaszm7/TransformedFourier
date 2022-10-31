9acc38531c634b998cbd30a7ddc1bf1abcf9d644
Lucas Morais <lucas.morais@visagio.com>
2022-10-31 17:26:55 -0300

    update

--- discrete_time_fourier_transform.m
+++ discrete_time_fourier_transform.m
@@ -1,23 +1,30 @@
-[data, freq] = audioread('StarWars60.wav');
+[data, N] = audioread('StarWars60.wav');
 
-#data = [];
-
-#for i=1 : 30
-#  data(i) = 0.8**(i-1)
+#sample_data = 0:0.1:30;
+#for i = 1 : length(sample_data)
+#  data(i) = 0.8^sample_data(i)
 #endfor
+#N = 300;
+#T = 1 / N;
 
 figure;
-plot(data);
+plot(sample_data, data);
 
+octave_fft = fft(data, 1 / T);
+
+figure;
+plot(abs(octave_fft));
+
+sample_dtft = -100:0.1:100
 dtft = [];
 
-for w=1 : 10
+for w = 1 : length(sample_dtft);
   dtft(w) = 0;
-  for n=1 : 10
-    dtft(w) += data(n) * exp(-i*w*n);
+  for n=1 : length(data)
+    dtft(w) += data(n) * exp(-j*sample_dtft(w)*n);
   endfor
 endfor
 
 figure;
-plot(dtft);
+plot(sample_dtft, abs(dtft));
 
