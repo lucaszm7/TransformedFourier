554f33c257da19989f64ec028ca8438d60b41a24
Lucas Morais <lucas.morais446@hotmail.com>
2022-11-03 16:39:55 -0300

    FINAL VERSION CLASS

--- dtft.m
+++ dtft.m
@@ -5,21 +5,23 @@ function [data_dtft, freq_dtft] = dtft (data, freq_data)
   segundos = n_amostras / freq_data;
   freq_dtft = aux / segundos;
 
-  data_dtft = fft(data);
+  data_dtft = fftshift(fft(data));
 
   figure();
   plot(freq_dtft, abs(data_dtft));
   title('FFTN - Dominio das Frequencias');
   xlabel('Frequencia (rad/s)');
   ylabel('Magnitude');
-
-  for w = 1 : n_amostras;
+  
+  for w = 1 : n_amostras
     data_dtft(w) = 0;
-    for n=1 : freq_data
-      data_dtft(w) += data(n) * exp(-j*w*n);
+    for n = 1 : n_amostras
+      data_dtft(w) += data(n) * exp(-j*(w-(n_amostras/2)-1)*(pi/(n_amostras))*(n-1));
     endfor
   endfor
 
+  length(data)
+  
   figure();
   plot(freq_dtft, abs(data_dtft));
   title('DTFT - Dominio das Frequencias');
